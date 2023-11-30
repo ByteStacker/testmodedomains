f8a60ba1829c5db85c67000df78ba53a1f7d7c59
ByteStacker <jd@modedomains.xyz>
2023-11-30 11:59:04 +0530

    update header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -38,14 +38,32 @@
         role="button"
       >
         {#if showMenu}
-          <img src={closeIcon} alt="modedomains logo" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            width="18"
+            viewBox="0 0 384 512"
+            ><path
+              fill="#ffffff"
+              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+            /></svg
+          >
         {:else}
-          <img src={menuIcon} alt="modedomains logo" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="16"
+            width="14"
+            viewBox="0 0 448 512"
+            fill="#ffffff"
+            ><path
+              d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+            /></svg
+          >
         {/if}
       </div>
       <div class="nav-links">
         <a class="nav-link" href="https://docs.modedomains.xyz/"> Docs </a>
-        <a class="nav-link" href="/profile"> Profile </a>
+        <a class="nav-link" href="/"> Profile </a>
       </div>
       <div class="cta-button"><w3m-button /></div>
     </div>
@@ -55,7 +73,7 @@
         <a class="nav-link" href="https://docs.modedomains.xyz/">
           <span class="nav-text-animate">Docs</span>
         </a>
-        <a class="nav-link" href="/profile">
+        <a class="nav-link" href="/">
           <span class="nav-text-animate">Profile</span>
         </a>
       </div>
